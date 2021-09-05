ccee82f11db43c161a10a2c7f3198e1a5e112f4d
Gabriel Valério de Souza <gabrielsouza4531@hotmail.com>
2021-09-05 17:18:06 -0300

    Add files via upload

---
+++ Calendário.java
@@ -0,0 +1,46 @@
+package Calendário;
+
+public class Calendário {
+
+	public static void main(String[] args) {
+		String vetor[] = new String[31];
+		int i = 0;
+		int nDias = 30; 
+		
+		vetor[0] = "SEGUNDA-FEIRA";
+		vetor[1] = "TERÇA-FEIRA";
+		vetor[2] = "QUARTA-FEIRA";
+		vetor[3] = "QUINTA-FEIRA";
+		vetor[4] = "SEXTA-FEIRA";
+		vetor[5] = "SABADO";
+		vetor[6] = "DOMINGO";
+		vetor[7] = "SEGUNDA-FEIRA";
+		vetor[8] = "TERÇA-FEIRA";
+		vetor[9] = "QUARTA-FEIRA";
+		vetor[10] = "QUINTA-FEIRA";
+		vetor[11] = "SEXA-FEIRA";
+		vetor[14] = "SABADO";
+		vetor[15] = "DOMINGO";
+		vetor[16] = "SEGUNDA-FEIRA";
+		vetor[17] = "TERÇA-FEIRA";
+		vetor[18] = "QUARTA-FEIRA";
+		vetor[19] = "QUINTA-FEIRA";
+		vetor[20] = "SEXTA-FEIRA";
+		vetor[21] = "SABADO";
+		vetor[22] = "DOMINGO";
+		vetor[23] = "SEGUNDA-FEIRA";
+		vetor[24] = "TERÇA-FEIRA";
+		vetor[25] = "QUARTA-FEIRA";
+		vetor[26] = "QUINTA-FEIRA";
+		vetor[27] = "SEXTA-FEIRA";
+		vetor[28] = "SABADO";
+		vetor[29] = "DOMINGO";
+		vetor[30] = "SEGUNDA-FEIRA";
+		
+		for (i = 1; i <= nDias; i++) {
+			System.out.println(i + "-" + vetor[i]);
+		}
+		
+}
+
+ }
